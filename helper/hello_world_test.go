6b04ddc1b5a527221e34d6fe4596b8f01eb2c42c
adamrayw <raywibowo68@gmail.com>
2023-02-16 22:42:23 +0700

    use ErrorF and Fatal function

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,9 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	// Calling the function `HelloWorld` with the parameter `Adam` and assigning the result to the
@@ -9,6 +12,25 @@ func TestHelloWorld(t *testing.T) {
 	// Checking if the result is not equal to "Hello Adam"
 	if result != "Hello Adam" {
 		// Throwing an error.
-		panic("Result is not Hello Adam")
+		// Test failed, but we can keep going
+		t.Errorf("Return is not Hello Adam")
 	}
+
+	// it will execute or continuing with the test
+	fmt.Println("TestHelloWorld done!")
+}
+
+func TestHelloWorldAdam(t *testing.T) {
+	// Calling the function `HelloWorld` with the parameter `Adam` and assigning the result to the
+	// variable `result`.
+	result := HelloWorld("Adam")
+	// Checking if the result is not equal to "Hello Adam"
+	if result != "Hello Adam" {
+		// Throwing an error.
+		// Test failed, and we have to stop
+		t.Fatal("Return is not Hello Adam")
+	}
+
+	// This code will never be executed
+	fmt.Println("TestHelloWorldAdam done!")
 }
