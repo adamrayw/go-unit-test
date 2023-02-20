7d35b1e2b8f5de0432aa8082f3d0f9ee9bf0c862
adamrayw <raywibowo68@gmail.com>
2023-02-20 22:25:53 +0700

    Category Entity

---
+++ entity/category.go
@@ -0,0 +1,9 @@
+package entity
+
+// Defining a struct called Category.
+type Category struct {
+	// @property {string} Id - The unique identifier for the category.
+	// @property {string} Name - The name of the category.
+	Id   string
+	Name string
+}
