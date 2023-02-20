a71c7be66de7806fb5a7846cf10b7a06c7579197
adamrayw <raywibowo68@gmail.com>
2023-02-20 21:39:25 +0700

    Add documentation on CategoryStruct

--- service/category_service.go
+++ service/category_service.go
@@ -1,7 +1,11 @@
 package service
 
-import "go-unit-test/repository"
+import (
+	"go-unit-test/repository"
+)
 
+// CategoryService is a struct with a field named Repository of type repository.CategoryRepository.
+// @property Repository - This is the repository that will be used to perform the database operations.
 type CategoryService struct {
 	Repository repository.CategoryRepository
 }
