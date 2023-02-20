a63b0be5c339296b86c19121a232110fccf8b506
adamrayw <raywibowo68@gmail.com>
2023-02-20 22:41:10 +0700

    add Get Method with return category and error

--- service/category_service.go
+++ service/category_service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+	"go-unit-test/entity"
 	"go-unit-test/repository"
 )
 
@@ -8,4 +10,17 @@ import (
 // @property Repository - This is the repository that will be used to perform the database operations.
 type CategoryService struct {
 	Repository repository.CategoryRepository
+}
+
+// A method that belongs to the CategoryService struct.
+func (service CategoryService) Get(id string) (*entity.Category, error) {
+	// Calling the FindById method of the CategoryRepository struct.
+	category := service.Repository.FindById(id)
+
+	// Checking if the category is nil and if it is, it returns an error.
+	if category == nil {
+		return category, errors.New("Category Not Found")
+	} else {
+		return category, nil
+	}
 }
